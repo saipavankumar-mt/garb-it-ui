44dd9021aa27f4544eda4fe3b13b15f4a9b37574
kalyanpraveen <kalyankerpraveen@gmail.com>
2021-01-24 01:50:39 +0530

    Add https in base url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://ec2-15-206-168-121.ap-south-1.compute.amazonaws.com/api'
+export const API_BASE_URL = 'https://ec2-15-206-168-121.ap-south-1.compute.amazonaws.com/api'
 
 export const GENDER = ['Male', 'Female', 'Transgender', 'None']
 
